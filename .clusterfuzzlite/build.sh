c7679405e504b738d77c464b2d100a90cdc0a883
Jacarte <javierca@kth.se>
2023-01-19 15:32:35 +0100

    WOrkaround for testing

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,16 +1,22 @@
 
 cd $SRC/knyst
 
+find / -name alsa 2>/dev/null
+find / -name "libasound*" 2>/dev/null
 
-cargo fuzz build --strip-dead-code -O --debug-assertions "$@"
+PATH="/usr/include:/usr/lib/x86_64-linux-gnu:$PATH" cargo fuzz build --strip-dead-code -O --debug-assertions "$@"
 FUZZ_TARGET_OUTPUT_DIR=target/x86_64-unknown-linux-gnu/release
+
+cp -r --parents /usr/lib/x86_64-linux-gnu/libasound* $OUT/
+cp -r --parents /usr/include/alsa $OUT/
 
 for f in fuzz/fuzz_targets/*.rs
 do
-    FUZZ_TARGET_NAME=$(basename ${f%.*})
+    FUZZ_TARGET_NAME="random_target" # copy only one for now$(basename ${f%.*})
     # Copy each compiled fuzz target to the output folder
     # the clusterfuzz lite backkend will execute each binary in the folder as a
     # fuzz target
     cp $FUZZ_TARGET_OUTPUT_DIR/$FUZZ_TARGET_NAME $OUT/
+    break
 done
 
