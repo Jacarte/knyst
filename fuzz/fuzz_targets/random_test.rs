7b8c8c3f1c6d61648b09f2c822ba836b177b323e
Jacarte <javierca@kth.se>
2023-01-19 10:48:03 +0100

    Adding gitignore for fuzz output

--- fuzz/fuzz_targets/random_test.rs
+++ fuzz/fuzz_targets/random_test.rs
@@ -6,4 +6,10 @@ extern crate libfuzzer_sys;
 // to merge with main
 fuzz_target!(|data: &[u8]| {
     println!("Hello from fuzz, data {:?}", data);
+
+    // Crash if data len > 3
+    //
+    if data.len() > 3 {
+        panic!("Test crash 1");
+    }
 });
